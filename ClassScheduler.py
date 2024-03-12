bcc9ed8cf77c371563cb26d40c623f6a52a9dd8b
pebblewebble <ericlye123344@gmail.com>
2024-03-12 11:19:41 +0800

    Small comments and changes to genome and thought process

--- ClassScheduler.py
+++ ClassScheduler.py
@@ -3,7 +3,7 @@ from typing import List
 from random import choices
 
 # For Genome, I am thinking of having a list containing a list of ints instead
-Genome = List[List[int]]
+Genome: List[List[int]] = [[] for _ in range(5)]
 Classes = namedtuple("Class", ["name", "duration", "value", "num_students"])
 Days = namedtuple("Day", ["name", "available_duration"])
 
@@ -24,8 +24,9 @@ def generate_population(size: int, genome_length: int):
 # Instead of weight limit, it should be overlaps for classes?
 # But having an overlap limit feels useless as you wouldn't want it to ever be larger than 0
 # Value was still be included just in case in the future I want to have a more favored class
+# Need to think of a way to handle sequences of the class
 def fitness(
-    genome: List[int], classes: List[Classes], days: List[Days], weight_limit: int
+    genome: Genome, classes: List[Classes], days: List[Days], weight_limit: int
 ) -> int:
     if len(genome) != len(classes):
         raise ValueError("Genome and Classes are supposed to be the same")
@@ -33,11 +34,12 @@ def fitness(
     duration_taken = 0
     value = 0
 
-    for i, classs in enumerate(classes):
-        if genome[i] == 1:
-            duration_taken += int(classs.duration)
-            value += int(classs.value)
+    for x, day in enumrate(days):
+        for i, classs in enumerate(classes):
+            if genome[i] == 1:
+                duration_taken += int(classs.duration)
+                value += int(classs.value)
 
-            if duration_taken > available_duration:
-                return 0
+                if duration_taken > available_duration:
+                    return 0
     return value
