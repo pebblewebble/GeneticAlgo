2e2ed2164abe56aadeab3efa74fcff49bcf42357
pebblewebble <ericlye123344@gmail.com>
2024-06-19 20:46:31 +0800

    Reformatted the genome so that each day has a list of binary instead of the classes themselves

--- ClassScheduler.py
+++ ClassScheduler.py
@@ -7,7 +7,8 @@ from random import choices
 # Genome: List[List[str]] = [[] for _ in range(5)]
 Class = namedtuple("Class", ["name", "duration", "value", "num_students"])
 Day = namedtuple("Day", ["name", "available_duration"])
-Genome = Dict[Day, List[Class]]
+Genome = Dict[Day, List[int]]
+Population = List[Genome]
 ClassesList = [
     Class("SPCC", "2", "5", "30"),
     Class("PSMOD", "2", "5", "30"),
@@ -19,23 +20,29 @@ ClassesList = [
 DaysList = [Day("Monday", "5"), Day("Tuesday", "10")]
 
 
-def generate_genome() -> Genome:
+def generate_genome() -> Genome :
     genome: Genome = defaultdict(list)
-    copyList = ClassesList.copy()
+    currentAvailableClass = [1 for _ in range(len(ClassesList))]
     for day in DaysList:
-        for cls in copyList:
-            if random.randint(0, 1) == 1:
-                genome[day].append(cls)
-                copyList.remove(cls)
-    return genome
+        for i in range(len(currentAvailableClass)):
+            if currentAvailableClass[i]==1:
+                if random.randint(0, 1) == 1:
+                    genome[day].append(1)
+                    #Since the class has been added into another day, you can remove it from being 
+                    #selected again
+                    currentAvailableClass[i]=0
+                else:
+                    genome[day].append(0)
+            else:
+                genome[day].append(0)
 
+    return genome
 
 def generate_population(size: int) -> List[Genome]:
     population = []
     for _ in range(size):
         population.append(generate_genome())
     return population
-
 
 # Thought process
 # What would replace weight limit for classes?
@@ -45,17 +52,25 @@ def generate_population(size: int) -> List[Genome]:
 # Need to think of a way to handle sequences of the class
 def fitness(genome: Genome, classes: List[Class], days: List[Day]) -> int:
     duration_taken = 0  
-    value = 0
+    fitnessValue = 0
     for day in genome:
-        classsesInDay = genome[day]
-        for index in range(len(classsesInDay)):
-            duration_taken += int(classsesInDay[index].duration)
-            value += int(classsesInDay[index].value)
+        for index in range(len(classes)):
+             if genome[day][index]==1:
+                duration_taken += int(classes[index].duration)
+                fitnessValue += int(classes[index].value)
 
-            if duration_taken>int(day.available_duration):
+             if duration_taken>int(day.available_duration):
                 return 0
-    print(value)
-    return value
+    return fitnessValue
 
+#After calculating their fitness, we can now select the strongest parents
+def selection_pair(population:Population) -> Population :
+    genomeWithFitness = [(currentGenome,fitness(currentGenome,ClassesList,DaysList)) for currentGenome in population]
+    #After giving them their fitness, we can now order them and then only take the top 2
+    sortedByFitness = sorted(genomeWithFitness,key=lambda x:x[1],reverse=True)
+    return [genome for genome,_ in sortedByFitness[:2]]
 
-fitness(generate_genome(), ClassesList, DaysList)
+population = generate_population(5)
+# # for currentGenome in population:
+for i in selection_pair(population):
+    print(i)
